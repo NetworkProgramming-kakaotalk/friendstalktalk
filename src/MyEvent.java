7fa3c2d6686ecff4a0545b0eb912d60ac66ff09f
leeyeeun <lye0308@iCloud.com>
2022-12-03 17:35:47 +0900

    thirdCommit

--- src/MyEvent.java
+++ src/MyEvent.java
@@ -6,13 +6,11 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-
 import java.awt.Rectangle;
+import java.awt.Window;
+
 import javax.swing.JTextField;
-import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
@@ -22,14 +20,16 @@ public class MyEvent extends JFrame implements ActionListener{
    JPanel p;
    private ShowList showlist1;
    private JTextField textField;
-   private JTextPane listArea;
-   private FriendsList friendslist;
+   private JTextField txtUserName;
+   private JTextField txtIpAddress;
+   private JTextField txtPortNumber;
+   public ChatWindow view;
    
-   public MyEvent(String username,String ip_addr,String port_no) {
+   public MyEvent(String username, String ip_addr, String port_no) {
       setTitle("친구 톡톡");
       setBounds(100,100,340,560);
       
-      //사용자 버튼
+    //사용자 버튼
       Color Gray= new Color(0xECECED);
       p = new JPanel();
       ImageIcon user_icon = new ImageIcon("src/user.png");
@@ -43,71 +43,57 @@ public class MyEvent extends JFrame implements ActionListener{
       b2.setBackground(Gray);
       b2.setBounds(12, 70, 80, 50);
       b2.setBorderPainted(false);
-
       b1.addActionListener(this);
       b2.addActionListener(this);
       p.setLayout(null);
       
-      p.add(b1);
-      p.add(b2);
-      getContentPane().add(p);
+      //b3 = new JButton("대화방 입장하기");
+      
+      
+      b2.addActionListener(new ActionListener () {
+      public void actionPerformed(ActionEvent e) {
+         if(b2.getText().equals("대화")) {
+               
+         view= new ChatWindow(username, ip_addr, port_no);
+         view.setBounds(400, 100, 392, 634);
+         //setBounds(100, 100, 392, 634);
+         b2.setText("화면닫기");
+
+         } else {
+            b2.setText("대화");
+            view.dispose();
+         }
+      }});
+      
+     p.add(b1);
+     p.add(b2);
+     getContentPane().add(p);
           
       
-      int y=50; // 클릭할때마다 y값 60씩 증가
-      	showlist1 = new ShowList(username);
-      	showlist1.setBackground(Color.WHITE);
-      	showlist1.setBounds(10, y, 210, 49);
-      	
-      	friendslist = new FriendsList(username, ip_addr, port_no);
-      	friendslist.setBackground(Color.WHITE);
-      	friendslist.setBounds(10, y, 210, 49);
+    
+      showlist1 = new ShowList(username);
+      showlist1.setBackground(Color.WHITE);
+      showlist1.setBounds(10, 50, 210, 364);
+     
       
-//          showlist2 = new ShowList(username);
-//          showlist2.setBackground(Color.WHITE);
-//          showlist2.setBounds(10, 110, 210, 49);
-//          
-//          showlist3 = new ShowList(username);
-//          showlist3.setBackground(Color.WHITE);
-//          showlist3.setBounds(10, 170, 210, 49);
-      	
       JPanel panel = new JPanel();
-      panel.setBounds(104,10,220,120);
+      panel.setBounds(104,10,220,511);
       panel.setBackground(Color.WHITE);
       panel.setLayout(null);
-    
-      
-      //listArea = new JTextPane();
-      //listArea.setEditable(true);
-      //listArea.setFont(new Font("굴림체", Font.PLAIN, 14));
-      //scrollPane.setViewportView(listArea);
-      //listArea.setBackground(Color.WHITE);
-      //listArea.setBorder(null);
       
       textField = new JTextField();
-      textField.setBounds(0, 0, 80, 40);
+      textField.setBounds(0, 0, 129, 40);
       textField.setBackground(new Color(255, 255, 255));
-      textField.setText("목록");
+      textField.setText("나의 프로필");
       textField.setEditable(false);
       textField.setHorizontalAlignment(SwingConstants.CENTER);
-	  textField.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+     textField.setFont(new Font("맑은 고딕", Font.BOLD, 20));
       panel.add(textField);
       textField.setColumns(10);
       panel.add(showlist1);
       showlist1.setLayout(null);
       p.add(panel);
       
-      JScrollPane scrollPane = new JScrollPane();
-      scrollPane.setBounds(104, 130, 220, 390);
-      scrollPane.setBorder(null);
-      p.add(scrollPane);
-      	
-      JPanel listPanel = new JPanel();
-      scrollPane.setViewportView(listPanel);
-      //listPanel.setBounds(104,10,220,120);
-      listPanel.setBackground(Color.WHITE);
-      listPanel.setLayout(null);
-      listPanel.setBorder(null);
-      listPanel.add(friendslist);
       
       setVisible(true);
       
@@ -116,20 +102,20 @@ public class MyEvent extends JFrame implements ActionListener{
    @Override
    public void actionPerformed(ActionEvent e) {
       if(e.getSource()==b1) {
-    	  textField.setText("목록");
-    	  textField.setHorizontalAlignment(SwingConstants.CENTER);
-    	  textField.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-    	  textField.setEditable(false);
-    	  showlist1.setVisible(true);
-    	  friendslist.setVisible(true);
+         textField.setText("나의 프로필");
+         textField.setHorizontalAlignment(SwingConstants.CENTER);
+         textField.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+         textField.setEditable(false);
+         showlist1.setVisible(true);
 
       }else if(e.getSource()==b2) {
-    	  textField.setText("대화창");
-    	  textField.setHorizontalAlignment(SwingConstants.CENTER);
-    	  textField.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-    	  textField.setEditable(false);
-    	  showlist1.setVisible(false);
-    	  friendslist.setVisible(false);
+         textField.setText("대화창");
+         textField.setHorizontalAlignment(SwingConstants.CENTER);
+         textField.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+         textField.setEditable(false);
+         showlist1.setVisible(false);
+         
+
       }
    }
 }
--- src/ShowList.java
+++ src/ShowList.java
@@ -1,61 +1,57 @@
-	import java.awt.*;
-	import java.text.SimpleDateFormat;
-	import javax.swing.*;
-	import java.util.Date;
-	import java.util.Vector;
-	import javax.swing.text.StyledDocument;
-	
-	public class ShowList extends JPanel {
-		private LogIn LogIn = new LogIn();
-		private JTextField txtHello;
-		private JTextField txtTime;
-		
-		
-		public ShowList(String username) {
-			setLayout(null);
-			
-			SimpleDateFormat formatter= new SimpleDateFormat("yyyy.MM.dd. HH'시' mm'분' ss'초'");
-			Date date = new Date(System.currentTimeMillis());
-	
-			System.out.println(System.currentTimeMillis());
-			System.out.println(formatter.format(date));
-			
-			txtHello = new JTextField();
-			txtHello.setText(username);
-			txtHello.setHorizontalAlignment(SwingConstants.LEFT);
-			txtHello.setFont(new Font("맑은 고딕", Font.BOLD, 15));
-			txtHello.setBackground(Color.WHITE);
-			txtHello.setBounds(50, 0, 170, 25);
-			txtHello.setEditable(false);
-			add(txtHello);
-			txtHello.setColumns(10);
-			
-			txtTime = new JTextField();
-			txtTime.setText(formatter.format(date));
-			txtTime.setBounds(50, 25, 170, 25);
-			txtTime.setBackground(Color.WHITE);
-			txtTime.setEditable(false);
-			add(txtTime);
-			txtTime.setColumns(10);
-			
-		}
-		/*
-		public Vector<Friend> FriendVector = new Vector<friend>();
-		
-		public void AddFriend(ImageIcon icon, String username, String userstatus, String statusmsg) {
-	        int len = textPanelFriendList.getDocumetn().getLength();
-	        textPanelFriendList.setCaretPosition(len);
-	        Friend f = new Friend(mainview, icon, username,userstatus, statusmsg);
-	        f.UserStatusMsg = statusmsg;
-	        textPaneFriendList.insertComponent(f);
-	        if (username.equals(UserName) ) {
-	           f.setBackground(new Color(240,240,240));
-	           f.SetStatusChangeActive();
-	        }
-	        f.SetProfileButtonActive();
-	        FriendVector.add(f);
-	        textPaneFriendList.setCaretPosition(0);
-	        repaint();
-	     }*/
-		
-	}
+import java.awt.*;
+import java.text.SimpleDateFormat;
+
+import javax.swing.*;
+import java.util.Date;
+
+public class ShowList extends JPanel {
+   private LogIn javaGameClientMain = new LogIn();
+   private JTextField txtHello;
+   private JTextField txtTime;
+   public ImageIcon icon1 = new ImageIcon("src/icon1.jpg");
+   
+   public ShowList(String username) {
+      setLayout(null);
+      
+      SimpleDateFormat formatter= new SimpleDateFormat("yyyy.MM.dd. HH'시' mm'분' ss'초'");
+      Date date = new Date(System.currentTimeMillis());
+
+      System.out.println(System.currentTimeMillis());
+      System.out.println(formatter.format(date));
+      
+      txtHello = new JTextField();
+      txtHello.setText(username);
+      txtHello.setHorizontalAlignment(SwingConstants.LEFT);
+      txtHello.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+      txtHello.setBackground(Color.WHITE);
+      txtHello.setBounds(55, 90, 170, 25);
+      txtHello.setEditable(false);
+      add(txtHello);
+      txtHello.setColumns(10);
+      
+      txtTime = new JTextField();
+      txtTime.setText(formatter.format(date));
+      txtTime.setBounds(55, 125, 170, 25);
+      txtTime.setBackground(Color.WHITE);
+      txtTime.setEditable(false);
+      add(txtTime);
+      txtTime.setColumns(10);
+      
+      JTextPane textPane = new JTextPane();
+      textPane.setText("사용자");
+      textPane.setEditable(false);
+      textPane.setBounds(0, 90, 55, 25);
+      add(textPane);
+      
+      JTextPane textPane_1 = new JTextPane();
+      textPane_1.setText("접속시간");
+      textPane_1.setEditable(false);
+      textPane_1.setBounds(0, 125, 55, 25);
+      add(textPane_1);
+      
+      JLabel iconLabel = new JLabel(icon1);
+      iconLabel.setBounds(70, 10, 80, 70);
+      add(iconLabel);
+      
+   }
+}
